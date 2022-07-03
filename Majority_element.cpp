19b7e6b94338f39d6496c7e6358a5deabf149cdc
aniru <anirudhsharma.3578@gmail.com>
2022-07-03 14:25:13 +0530

    Majority Element-1

---
+++ Majority_element.cpp
@@ -0,0 +1,41 @@
+//Link:- https://leetcode.com/problems/majority-element/submissions/ 
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        /* Moore's Voting Algorithm
+        int count=0;
+        int candidate=0;
+        
+        for(int num:nums)
+        {
+            if(count==0)
+            {
+                candidate=num;
+            }    
+            else if(num==candidate)
+            {
+                count +=1;
+            }    
+            else
+            {
+                count -=1;
+            }    
+        }    
+        
+        return candidate;*/
+        // Hash Map Approach
+        
+        unordered_map<int,int> counter;
+        
+        for(int num:nums)
+        {
+            if(++counter[num]>nums.size()/2)
+            {
+                return num;
+            }    
+        }    
+        
+        return 0;
+        
+    }
+};
