7d73a717887733d91dcdac4da6a5b8d3e90d38d7
aniru <anirudhsharma.3578@gmail.com>
2022-07-05 03:22:35 +0530

    Longest Consecutive

---
+++ LongestConsecutiveSequence.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        //Brute Force
+//         if(nums.size() == 0 ){
+//             return 0;
+//         }
+        
+//         sort(nums.begin(),nums.end());
+        
+//         int ans = 1;
+//         int prev = nums[0];
+//         int cur = 1;
+        
+//         for(int i = 1;i < nums.size();i++){
+//             if(nums[i] == prev+1){
+//                 cur++;
+//             }
+//             else if(nums[i] != prev){
+//                 cur = 1;
+//             }
+//             prev = nums[i];
+//             ans = max(ans, cur);
+//         }
+//         return ans;
+        
+        // Optimized Solution - Hashset approach
+        set<int> hashSet;
+        for(int num:nums)
+        {
+            hashSet.insert(num);
+        }    
+        int longestStreak=0;
+        for(int num:nums)
+        {
+            if(!hashSet.count(num-1))
+            {
+                int currentNum=num;
+                int currentStreak=1;
+            
+            while(hashSet.count(currentNum+1))
+            {
+                currentNum+=1;
+                currentStreak+=1;
+            }    
+            
+            longestStreak=max(longestStreak, currentStreak);
+        } 
+    }
+        return longestStreak;
+    }
+};
